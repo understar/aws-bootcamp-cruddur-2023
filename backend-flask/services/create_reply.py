e707c79710f2e72ce4d5495541a8a8cd668f3824
Shuaiyi Zhang <shuaiyigis@gmail.com>
2023-12-07 00:10:16 +0000

    X-ray segement and subsegment

--- backend-flask/services/create_reply.py
+++ backend-flask/services/create_reply.py
@@ -1,7 +1,20 @@
 import uuid
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
+
 class CreateReply:
   def run(message, user_handle, activity_uuid):
+    # x-ray
+    # Start a segment
+    segment = xray_recorder.begin_segment('create_reply')
+    
+    # Start a subsegment
+    subsegment = xray_recorder.begin_subsegment('check_reply')
+
+    # Add metadata or annotation here if necessary
+    # Metadata won't be used for filter
+    # segment.put_metadata('key', dict, 'namespace')
+
     model = {
       'errors': None,
       'data': None
@@ -9,14 +22,22 @@ class CreateReply:
 
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['user_handle_blank']
+      subsegment.put_annotation('reply_error', 'user_handle_blank')
 
     if activity_uuid == None or len(activity_uuid) < 1:
       model['errors'] = ['activity_uuid_blank']
+      subsegment.put_annotation('reply_error', 'activity_uuid_blank')
 
     if message == None or len(message) < 1:
-      model['errors'] = ['message_blank'] 
+      model['errors'] = ['message_blank']
+      subsegment.put_annotation('reply_error', 'message_blank')
+      
     elif len(message) > 1024:
-      model['errors'] = ['message_exceed_max_chars'] 
+      model['errors'] = ['message_exceed_max_chars']
+      subsegment.put_annotation('reply_error', 'message_exceed_max_chars')
+
+    # End subsegment
+    xray_recorder.end_subsegment()
 
     if model['errors']:
       # return what we provided
@@ -28,6 +49,7 @@ class CreateReply:
       }
     else:
       now = datetime.now(timezone.utc).astimezone()
+      segment.put_annotation("now", now.isoformat())
       model['data'] = {
         'uuid': uuid.uuid4(),
         'display_name': 'Andrew Brown',
@@ -36,4 +58,7 @@ class CreateReply:
         'created_at': now.isoformat(),
         'reply_to_activity_uuid': activity_uuid
       }
+    
+    # Close the segment
+    xray_recorder.end_segment()
     return model
