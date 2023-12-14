337b7d75f15d293ca003368d55789d2ad1d4433b
ShuaiYi Zhang <shuaiyigis@gmail.com>
2023-12-14 17:56:58 +0000

    Switch to codespaces

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/bash
+
+curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
+sudo apt update
+sudo apt install -y postgresql-client-13 libpq-dev
+
+export CODESPACE_IP=$(curl ifconfig.me)
+source "$CODESPACE_VSCODE_FOLDER/backend-flask/db-update-sg-rule"
+
+cd $CODESPACE_VSCODE_FOLDER/backend-flask
+pip install -r requirements.txt
+
+cd $CODESPACE_VSCODE_FOLDER/frontend-react-js
+npm i
+
+exit 0
